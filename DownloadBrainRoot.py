097bacb4c9afd6be9b8a35492bf3b7c4e7dbfcda
Almighty-Peter <150950929+Almighty-Peter@users.noreply.github.com>
2024-08-19 16:50:01 +0200

    Update DownloadBrainRoot.py

--- DownloadBrainRoot.py
+++ DownloadBrainRoot.py
@@ -1,6 +1,6 @@
 import yt_dlp
 # URL of the YouTube video
-videoIds = ["3wd9RcZ2fHc"]
+videoIds = ["3wd9RcZ2fHc","aw_zjI9ouLU","jeFc7xmtbx4","c1jHesb49Z8","9mzjYfztNp0","LMsPUPZi9M8","OKmWPlsEbZo"]
 
 # Output directory
 download_path = "/Users/peternyman/Clips/BrainRoot/"
@@ -10,7 +10,7 @@ def download_video(videoId):
     url = f'https://www.youtube.com/watch?v={videoId}'
     
     ydl_opts = {
-        'format': 'bestvideo[vcodec=h264]+bestaudio[acodec=aac]/best',  # Specify H.264 and AAC codecs
+        'format': 'bestvideo+bestaudio',  # Specify H.264 and AAC codecs
         'outtmpl': f'{download_path}{videoId}.%(ext)s',  # Output template
         'merge_output_format': 'mp4',  # Ensure the final format is mp4
     }
