1f1258fcdf15ea972893a58cf474456db0a78e8f
Almighty-Peter <150950929+Almighty-Peter@users.noreply.github.com>
2024-09-08 18:45:24 +0200

    Update ChatGpt.py

--- ChatGpt.py
+++ ChatGpt.py
@@ -1,11 +1,13 @@
 from openai import OpenAI
+from pathlib import Path
+
 
 api_key=''
 client = OpenAI(api_key=api_key)
 
-def textToText(prompt,system_message):
+def textToText(prompt,system_message,model="gpt-4o-mini"):
     completion = client.chat.completions.create(
-      model="gpt-4o-mini",
+      model=model,
       messages=[
         {"role": "system", "content": system_message},
         {"role": "user", "content": prompt}
@@ -15,5 +17,18 @@ def textToText(prompt,system_message):
     return(completion.choices[0].message.content)
 
 def get_embedding(text, model="text-embedding-3-large"):
-   text = text.replace("\n", " ")
-   return client.embeddings.create(input = [text], model=model).data[0].embedding
+    text = text.replace("\n", " ")
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+def textToAudio(prompt):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=prompt
+    )
+    
+    audio_filename = "generated_audio.mp3"
+    with open(audio_filename, 'wb') as audio_file:
+        audio_file.write(response.content)
+    
+    return audio_filename
